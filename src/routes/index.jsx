33ed530c23db00b700fe23f1d4d1f10503e53b5d
Diego R R Silva <165964850+Decksx7@users.noreply.github.com>
2024-09-19 19:15:53 -0300

    add Audio Preview

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 
 import {DefaultLayout} from "../layout/DefaultLayout"
- 
 import {Home} from "../pages/Home"
 import {Favorites} from "../pages/Favorites"
 import {NotFound} from "../pages/NotFound"
 import { Api } from "../pages/api"
 import { PegarMusica } from "../pages/PegarMusica"
+
+
 
 export function Routers() {
   return (
@@ -14,10 +15,11 @@ export function Routers() {
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
+        
         <Route path="*" element={<NotFound />} />
-        <Route path="/api" element={<Api/>}/>
-        <Route path="/pegarmusica" element={<PegarMusica/>}/>
+        <Route path="/api" element={<Api />} />
+        <Route path="/pegarmusica" element={<PegarMusica />} />
       </Route>
     </Routes>
-  )
+  );
 }
