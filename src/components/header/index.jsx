d8b2dcac9bb0b500e6a85b17cecb040fb5d490a3
Diego R R Silva <165964850+Decksx7@users.noreply.github.com>
2024-09-19 21:05:13 -0300

    add storage

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,136 @@
-import { MagnifyingGlass, Student } from "phosphor-react";
-import { Gear, Star } from "phosphor-react";
-import { Bell } from "phosphor-react";
-import { List } from "phosphor-react";
+// import { MagnifyingGlass} from "phosphor-react";
+// import { Gear, Star } from "phosphor-react";
+// import { Bell } from "phosphor-react";
+// import { List } from "phosphor-react";
+// import img from "./img/music.png";
+// import styles from "./header.module.css";
+// import { useState, useEffect } from "react";
+// import { api } from "../../services/api";
+// export function Header() {
+//   const [dado, setdado] = useState("");
+//   const [datas, setdatas] = useState([]);
+
+//   const token =
+//     "BQAq_fqfJZk29Bd-4mUEVfEWzeh_wq4hPdtiWnohKEGNbUCznV-RXv0iMzZ-YWsuxAa-BPVRjafOriqXWYeSjLLWifsPw0k5ft7GR6_9SzUov4izXk4";
+
+//   function handleChangeSearchValue(e) {
+//     setdado(e.target.value);
+//   }
+
+//   useEffect(() => {
+//     console.log("data aqui ", datas);
+//     console.log(datas.length);
+
+   
+//   }, [datas]);
+//   async function handleSubmit() {
+
+
+//     const { data } = await api.get(`/v1/search?q=${dado}&type=track`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+
+//     console.log(data);
+//     console.log(data.albums);
+
+//     const dito = data.tracks.items.map((item) => item);
+
+//     setdatas(dito);
+//   }
+
+//   return (
+//     <>
+//       <header className={styles.header}>
+//         <div className={styles["left-container"]}>
+//           <img src={img} alt="Music" className={styles.iconImage} />
+
+//           <div className={styles.searchBar}>
+//             <MagnifyingGlass size={20} className={styles.icon} />
+//             <input
+//               type="text"
+//               placeholder="Search for songs, artists, etc."
+//               className={styles.input}
+//               onChange={handleChangeSearchValue}
+//               value={dado}
+//               onKeyDown={(e) => {
+//                 if (e.key === "Enter") {
+//                   handleSubmit();
+//                 }
+//               }}
+//             />
+//           </div>
+//         </div>
+
+//         <div className={styles["right-container"]}>
+//           <button className={styles.premiumButton}>Explore Premium</button>
+//           <div className={styles.iconsGroup}>
+//             <button>
+//               <Gear size={30} className={styles.iconGear} />
+//             </button>
+//             <button>
+//               <Bell size={30} className={styles.iconBell} />
+//             </button>
+//             <button>
+//               <List size={30} className={styles.iconList} />
+//             </button>
+//           </div>
+//         </div>
+//       </header>
+
+//       <main>
+//         <div className={styles.musics}>
+//           <form onSubmit={handleSubmit}>
+//             {datas.length > 0 && (
+//               <ul className={styles.album}>
+//                 {datas.map((data, index) => (
+//                   <li key={index} style={{ "margin-top": "20px" }}>
+//                     <img
+//                       className={styles.img1}
+//                       src={data.album.images[0].url}
+//                       alt={data.album.name}
+//                     />
+//                     <section className={styles.content}>
+//                       <div className={styles.textContent}>
+//                         <h1>{data.name}</h1>
+//                         <p>{data.artists[0].name}</p>
+//                         <div className={styles.favorite}>
+//                           <audio controls>
+//                             <source src={data.preview_url} type="audio/mpeg" />
+//                             Your browser does not support the audio element.
+//                           </audio>
+//                           <button>
+//                             <Star size={32} color="#c8cb1a" weight="fill" />
+//                           </button>
+//                         </div>
+//                       </div>
+//                     </section>
+//                   </li>
+//                 ))}
+//               </ul>
+//             )}
+//           </form>
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
+
+
+import { MagnifyingGlass, Star } from "phosphor-react";
+import { Gear, Bell, List } from "phosphor-react";
 import img from "./img/music.png";
 import styles from "./header.module.css";
 import { useState, useEffect } from "react";
 import { api } from "../../services/api";
+import { STORAGE_SERVICE } from "../../services/storage"; // Verifique se o caminho está correto
+
 export function Header() {
   const [dado, setdado] = useState("");
   const [datas, setdatas] = useState([]);
-
   const token =
-    "BQAq_fqfJZk29Bd-4mUEVfEWzeh_wq4hPdtiWnohKEGNbUCznV-RXv0iMzZ-YWsuxAa-BPVRjafOriqXWYeSjLLWifsPw0k5ft7GR6_9SzUov4izXk4";
+    "BQAipGBVo_l3lkmbOn2B2cQDLfmDUp4UadamuwVZGEn7SlN8Or8i9m-woDYPCWSjsgxPoHUDbYjGHPIOBppjN7HkdJ8JmCuHIemfcUscuPPK20TyLYk";
 
   function handleChangeSearchValue(e) {
     setdado(e.target.value);
@@ -20,11 +139,10 @@ export function Header() {
   useEffect(() => {
     console.log("data aqui ", datas);
     console.log(datas.length);
-
-    // console.log("este sao os albuns", datas.albums.items[0].name)
   }, [datas]);
-  async function handleSubmit() {
-    // e.preventDefault();
+
+  async function handleSubmit(e) {
+    e.preventDefault(); // Previne o comportamento padrão do formulário
 
     const { data } = await api.get(`/v1/search?q=${dado}&type=track`, {
       headers: {
@@ -35,10 +153,18 @@ export function Header() {
     console.log(data);
     console.log(data.albums);
 
-    const dito = data.tracks.items.map((item) => item);
+    const tracks = data.tracks.items.map((item) => item);
 
-    setdatas(dito);
+    setdatas(tracks);
+    
   }
+
+  const addSongToPlaylist = (songName) => {
+    STORAGE_SERVICE.createContact(songName);
+    alert(`Seu som foi adicionado a playlist!`); 
+  };
+
+
 
   return (
     <>
@@ -56,7 +182,7 @@ export function Header() {
               value={dado}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
-                  handleSubmit();
+                  handleSubmit(e);
                 }
               }}
             />
@@ -64,7 +190,7 @@ export function Header() {
         </div>
 
         <div className={styles["right-container"]}>
-          <button className={styles.premiumButton}>Explore Premium</button>
+          <button  className={styles.premiumButton}>Favoritos</button>
           <div className={styles.iconsGroup}>
             <button>
               <Gear size={30} className={styles.iconGear} />
@@ -82,17 +208,10 @@ export function Header() {
       <main>
         <div className={styles.musics}>
           <form onSubmit={handleSubmit}>
-            {/* <input
-              onChange={handleChangeSearchValue}
-              type="text"
-              value={dado}
-            />
-            <button type="submit">enviar</button> */}
-
             {datas.length > 0 && (
               <ul className={styles.album}>
                 {datas.map((data, index) => (
-                  <li key={index} style={{ "margin-top": "20px" }}>
+                  <li key={index} style={{ marginTop: "20px" }}>
                     <img
                       className={styles.img1}
                       src={data.album.images[0].url}
@@ -107,7 +226,7 @@ export function Header() {
                             <source src={data.preview_url} type="audio/mpeg" />
                             Your browser does not support the audio element.
                           </audio>
-                          <button>
+                          <button onClick={() => addSongToPlaylist(data)}>
                             <Star size={32} color="#c8cb1a" weight="fill" />
                           </button>
                         </div>
